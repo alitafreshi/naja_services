e067250897a5100d8436f87c9a883b39cb696dd8
ali tafreshi <alitafreshi123@yahoo.com>
2022-07-11 09:08:34 +0430

    Done

--- naja-services/src/main/java/ir/tafreshiali/naja_services/callback/CallBackHelper.kt
+++ naja-services/src/main/java/ir/tafreshiali/naja_services/callback/CallBackHelper.kt
@@ -4,28 +4,34 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import ir.tafreshiali.naja_services.constance.NajaConstance
 
-fun  <T> AppCompatActivity.handleNajaGatewayCallBackUrl(
-    url: String,
+/**Naja Gateway Call Back
+ * steps goes like down =
+ * 1 - first we use [substringAfter] with (?) [delimiter] and [split] the url with (&)
+ * 2 - then we are searching for (product) Keyword and [split] it with (=) [delimiter] in the founded url in first step.
+ * 3 - then we are searching for (key) Keyword and [split] it with (=) [delimiter] in the founded url in first step.
+ * 4 - if the value of the (product) Keyword that founds in step 2 is contains [NajaConstance.TRAFFIC_BY_PLATE] we are passing the required information with in an intent */
+fun <T> AppCompatActivity.handleNajaGatewayCallBackUrl(
     destinationActivity: Class<T>
 ) {
+    intent.data?.toString()?.let { url ->
+
+        val items = url.substringAfter("?").split("&")
+
+        val product = items.firstOrNull { it.startsWith("product") }?.split("=")?.get(1)
+
+        val key = items.firstOrNull { it.startsWith("key") }?.split("=")?.get(1)
+
+        if (product == NajaConstance.TRAFFIC_BY_PLATE) {
+
+            startActivity(Intent(this, destinationActivity).also { intent ->
+                intent.putExtra(NajaConstance.NAJA_CALL_BACK_PRODUCT, product)
+                intent.putExtra(NajaConstance.KEY, key)
+            })
 
 
-    val items = url.substringAfter("?").split("&")
-
-    val product = items.firstOrNull { it.startsWith("product") }?.split("=")?.get(1)
-
-    val key = items.firstOrNull { it.startsWith("key") }?.split("=")?.get(1)
-
-    if (product == NajaConstance.TRAFFIC_BY_PLATE) {
-
-        startActivity(Intent(this,destinationActivity).also { intent ->
-            intent.putExtra(NajaConstance.NAJA_CALL_BACK_PRODUCT, product)
-            intent.putExtra(NajaConstance.KEY, key)
-        })
-
-
-    } else {
-        finish()
-        startActivity(Intent(this, destinationActivity))
+        } else {
+            finish()
+            startActivity(Intent(this, destinationActivity))
+        }
     }
 }
