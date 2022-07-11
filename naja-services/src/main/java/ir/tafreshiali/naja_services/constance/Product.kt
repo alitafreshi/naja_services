c86feb411fd7da69918df57ff937a31a0534b8f9
ali tafreshi <alitafreshi123@yahoo.com>
2022-07-11 15:39:39 +0430

    Done

--- naja-services/src/main/java/ir/tafreshiali/naja_services/constance/Product.kt
+++ naja-services/src/main/java/ir/tafreshiali/naja_services/constance/Product.kt
@@ -3,5 +3,6 @@ package ir.tafreshiali.naja_services.constance
 object Product {
     const val InquiryNajaPlateNumbers = "InquiryNajaPlateNumbers"
     const val InquiryDrivingNegativePoint = "InquiryDrivingNegativePoint"
+    const val InquiryNajaDrivingLicenceNegativePoint = "InquiryNajaDrivingLicenceNegativePoint"
 
 }
